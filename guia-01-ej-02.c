892a634133822a202939b7423ba0c61a86754e3c
Laura E Morea <laura.morea.inspt@gmail.com>
2026-04-19 19:57:07 -0300

    Terminado

--- guia-01-ej-02.c
+++ guia-01-ej-02.c
@@ -3,23 +3,25 @@
 /*Serie de Fibonacci 
 Escriba un programa que imprima los primeros N términos de la serie de Fibonacci 
 utilizando recursividad.*/
-void Fibonacci(int N, int *a, int *b, int *c);
+void fibonacci(int n, int *a, int *b, int *c);
 
 int main(int argc, char const *argv[])
 {
-    int x, *a=0,*b=1, *c;
+    int x, a,b,c;
+    a=0;
+    b=1;
     printf("Ingrese la cantidad de terminos que quiera que se muestren: ");
     scanf("%d",&x);
-    Fibonacci(x,&a,&b,&c);
+    fibonacci(x,&a,&b,&c);
     return 0;
 }
 
-void Fibonacci(int N, int *a, int *b, int *c){
-    if(N>0){
+void fibonacci(int n, int *a, int *b, int *c){
+    if(n>0){
         printf("%d ",*b);
         *c=*b;
         *b=((*a)+(*b));
         *a=*c;
-        Fibonacci(N-1,&a,&b,&c);
+        fibonacci(n-1,a,b,c);
     }
     }
